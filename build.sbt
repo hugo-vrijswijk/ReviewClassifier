205634a767a75abbdfbd7eeaf848006f25ec8335
Hugo van Rijswijk <hugo.v.rijswijk@gmail.com>
2016-12-07 19:25:33 +0100

    Update gitignore and add example Naive Bayes

--- build.sbt
+++ build.sbt
@@ -5,8 +5,8 @@ version := "1.0"
 scalaVersion := "2.11.8"
 
 libraryDependencies ++= Seq(
-  "net.tixxit" %% "delimited-core" % "0.8.0",
-  "org.apache.spark" % "spark-core-2.11" % "2.0.2",
+  "net.tixxit" % "delimited-core_2.11" % "0.8.0",
+  "org.apache.spark" % "spark-core_2.11" % "2.0.2",
   "org.apache.spark" % "spark-mllib_2.11" % "2.0.2"
 //  "com.github.fommil.netlib" % "all" % "1.1.2",
 //  "org.scalanlp" %% "breeze" % "0.12",
@@ -15,6 +15,4 @@ libraryDependencies ++= Seq(
 )
 
 resolvers += "Sonatype Releases" at "https://oss.sonatype.org/content/repositories/releases/"
-
-
 
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,13 +1,16 @@
 import java.io.File
 
 import net.tixxit.delimited.{DelimitedFormat, DelimitedParser}
-import org.apache.spark.ml.classification.NaiveBayes
-
+import org.apache.spark
+import org.apache.spark.mllib.classification.{NaiveBayes, NaiveBayesModel}
+import org.apache.spark.mllib.util.MLUtils
+import org.apache.spark.{SparkConf, SparkContext}
 
 /**
   * Created by Hugo van Rijswijk on 01-Dec-16.
   */
 object Main extends App {
+  val sc = SparkContext.getOrCreate()
   val stream = getClass.getResourceAsStream("/labeledTrainData.tsv")
   val myFormat: DelimitedFormat = DelimitedFormat(
     separator = "\t",
@@ -32,4 +35,27 @@ object Main extends App {
     clean(text).split("[ ,!.]+").groupBy(identity).mapValues(_.length)
 
   def clean(text: String): String = text.replaceAll("<.*?>", "").toLowerCase
+
+
+
+
+  //Example
+  val conf = new SparkConf().setAppName("NaiveBayesExample")
+  val sc = new SparkContext(conf)
+  // $example on$
+  // Load and parse the data file.
+  val data = MLUtils.loadLibSVMFile(sc, "data/mllib/sample_libsvm_data.txt")
+
+  // Split data into training (60%) and test (40%).
+  val Array(training, test) = data.randomSplit(Array(0.6, 0.4))
+
+  val model = NaiveBayes.train(training, lambda = 1.0, modelType = "multinomial")
+
+  val predictionAndLabel = test.map(p => (model.predict(p.features), p.label))
+  val accuracy = 1.0 * predictionAndLabel.filter(x => x._1 == x._2).count() / test.count()
+
+  // Save and load model
+  model.save(sc, "target/tmp/myNaiveBayesModel")
+  val sameModel = NaiveBayesModel.load(sc, "target/tmp/myNaiveBayesModel")
+  // $example off$
 }
